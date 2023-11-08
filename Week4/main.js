c3d6005c76b202ff743985239afd761ac9fea4bd
MaksimMuzychenko <mm2148@hw.ac.uk>
2023-11-08 23:02:17 +0400

    edited json

--- Week4/main.js
+++ Week4/main.js
@@ -1,5 +1,5 @@
 var ourRequest = new XMLHttpRequest(); 
-ourRequest.open('GET', 'https://github.com/MaksimMuzychenko/MaksimMuzychenko.github.io/Week4/cities1.json'); 
+ourRequest.open('GET', 'https://maksimmuzychenko.github.io/Week4/cities1.json'); 
 ourRequest.onload = function() { 
 console.log(ourRequest.responseText); 
 }; 
