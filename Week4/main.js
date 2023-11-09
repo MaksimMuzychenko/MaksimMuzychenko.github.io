d5fcb2c222cc7294a071ec9f6979215944a4f88b
MaksimMuzychenko <mm2148@hw.ac.uk>
2023-11-09 19:08:24 +0400

    changed link

--- Week4/main.js
+++ Week4/main.js
@@ -2,7 +2,7 @@ var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn"); 
 btn.addEventListener("click", function(){ 
 var ourRequest = new XMLHttpRequest(); 
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repository-name/cities1.json'); 
+ourRequest.open('GET', 'https://MaksimMuzychenko.github.io/Week4/cities1.json'); 
 ourRequest.onload = function() { 
 var ourData = JSON.parse(ourRequest.responseText); 
 renderHTML(ourData); 
