d2037830024a389b4ba71534378104bb63182523
MaksimMuzychenko <mm2148@hw.ac.uk>
2023-11-09 18:20:36 +0400

    changed all the names for the Ourrequest variable

--- Week4/main.js
+++ Week4/main.js
@@ -2,6 +2,6 @@ var xml = new XMLHttpRequest();
 console.log(xml.readyState);
 xml.open('GET', 'https://maksimmuzychenko.github.io/Week4/cities1.json'); 
 xml.onload = function() { 
-console.log(ourRequest.responseText); 
+console.log(xml.responseText); 
 }; 
 xml.send(); 
